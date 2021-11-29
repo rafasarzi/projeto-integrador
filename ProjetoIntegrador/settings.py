aeba44758c6681507be58937c07fb79b7e9deca2
Rafaela Sarzi <rajustino@gmail.com>
2021-11-29 16:28:51 -0300

    Autenticação de login

--- ProjetoIntegrador/settings.py
+++ ProjetoIntegrador/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'paginas.apps.PaginasConfig',
+    'usuarios.apps.UsuariosConfig',
     'crispy_forms',
     'crispy_bootstrap5',
 ]
@@ -134,3 +135,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configs de autenticação
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
--- paginas/views.py
+++ paginas/views.py
@@ -4,6 +4,7 @@ from django.views.generic.list import ListView
 from django.views.generic import TemplateView
 from paginas.models import CadastroAlunos, CadastroProfissional, CadastroTerapia
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -32,21 +33,24 @@ class SobreView(TemplateView):
 #     template_name = "paginas/caixa.html"
 
 
-class CadastroCreate(CreateView):
+class CadastroCreate(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     template_name = 'paginas/cadastro.html'
     model = CadastroAlunos
     fields = ['nome', 'cpf', 'idade', 'email', 'telefone']
     success_url = reverse_lazy('listar-aluno')
 
 
-class ProfissionalCreate(CreateView):
+class ProfissionalCreate(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     template_name = 'paginas/cadastro.html'
     model = CadastroProfissional
     fields = ['nome', 'cpf', 'email', 'telefone', 'terapia']
     success_url = reverse_lazy('listar-profissional')
 
 
-class TerapiaCreate(CreateView):
+class TerapiaCreate(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     template_name = 'paginas/cadastro.html'
     model = CadastroTerapia
     fields = ['terapia', 'descrição']
@@ -55,21 +59,24 @@ class TerapiaCreate(CreateView):
 ########## UPDATE ##########
 
 
-class CadastroUpdate(UpdateView):
+class CadastroUpdate(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
     template_name = 'paginas/cadastro.html'
     model = CadastroAlunos
     fields = ['nome', 'cpf', 'idade', 'email', 'telefone']
     success_url = reverse_lazy('listar-aluno')
 
 
-class ProfissionalUpdate(UpdateView):
+class ProfissionalUpdate(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
     template_name = 'paginas/cadastro.html'
     model = CadastroProfissional
     fields = ['nome', 'cpf', 'email', 'telefone', 'terapia']
     success_url = reverse_lazy('listar-profissional')
 
 
-class TerapiaUpdate(UpdateView):
+class TerapiaUpdate(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
     template_name = 'paginas/cadastro.html'
     model = CadastroTerapia
     fields = ['terapia', 'descrição']
@@ -78,19 +85,22 @@ class TerapiaUpdate(UpdateView):
 ########## DELETE ##########
 
 
-class CadastroDelete(DeleteView):
+class CadastroDelete(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     template_name = 'paginas/cadastro-excluir.html'
     model = CadastroAlunos
     success_url = reverse_lazy('listar-aluno')
 
 
-class ProfissionalDelete(DeleteView):
+class ProfissionalDelete(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     template_name = 'paginas/cadastro-excluir.html'
     model = CadastroProfissional
     success_url = reverse_lazy('listar-profissional')
 
 
-class TerapiaDelete(DeleteView):
+class TerapiaDelete(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     template_name = 'paginas/cadastro-excluir.html'
     model = CadastroTerapia
     success_url = reverse_lazy('listar-terapia')
@@ -98,16 +108,19 @@ class TerapiaDelete(DeleteView):
 ########## LISTAR ##########
 
 
-class CadastroList(ListView):
+class CadastroList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
     template_name = 'paginas/listas/alunos.html'
     model = CadastroAlunos
 
 
-class ProfissionalList(ListView):
+class ProfissionalList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
     template_name = 'paginas/listas/professores.html'
     model = CadastroProfissional
 
 
-class TerapiaList(ListView):
+class TerapiaList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
     template_name = 'paginas/listas/aulas.html'
     model = CadastroTerapia
