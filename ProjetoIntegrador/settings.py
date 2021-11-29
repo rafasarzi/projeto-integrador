32f8b09dc9c46666c3fbcfb64ddf47c8df6e22b3
Rafaela Sarzi <rajustino@gmail.com>
2021-11-29 13:32:52 -0300

    ajustes css

--- ProjetoIntegrador/settings.py
+++ ProjetoIntegrador/settings.py
@@ -39,8 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'paginas.apps.PaginasConfig',
     'crispy_forms',
+    'crispy_bootstrap5',
 ]
-CRISPY_TEMPLATE_PACK = 'bootstrap'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
